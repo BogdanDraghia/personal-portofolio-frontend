19b37d5d4111bb00ab4312b1dc9f861cd2ddb730
bogdandraghia <bgd.draghia@gmail.com>
2022-04-22 10:42:26 +0000

    added gedsports widget

---
+++ pages/testgedsports.js
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import Script from 'next/script'
+import { useEffect } from 'react'
+
+
+
+
+const testgedsport = ()=>{
+
+
+
+    return (
+        <>
+     <Head>
+        <script src="/run.js"></script>
+      </Head>
+      </>
+    )
+}
+
+export default testgedsport 

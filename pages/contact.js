a9cacd7a2ee9aefaf0fc1f12df1f40599262ca9d
bogdandraghia <bgd.draghia@gmail.com>
2022-08-07 15:40:31 +0000

    contact page mobile view, variables

--- pages/contact.js
+++ pages/contact.js
@@ -10,60 +10,67 @@ const Contact = () => {
   return (
     <div className={style.contactContainer}>
       <div className={style.subContactContainer}>
-        <div className={style.contactBox}>
-          <div className={style.iconsBox} >
-            <div className={style.iconComponent}>
+      <div className={style.contactBoxTextContainer}>
+          <div className={style.contactBoxBoxTextSubContainer}>
+            <h1>
+            Get in touch
+            </h1>
+            <p>If you want to create something, don't hesitate to contact </p>
+          </div>
+      </div>
+
+      <div className={style.contactBox}>
+        <div className={style.iconsBox} >
+          <div className={style.iconComponent}>
             <SvgIconHelper name="linkedin" icon={<LinkedinComponent  />} pass={"https://github.com/bogdandraghia"} />
-
-            </div>
-            <div className={style.iconComponent}>
-            <div className={style.iconComponent}>
-
+          </div>
+          <div className={style.iconComponent}>
             <SvgIconHelper name="twitter" icon={<Email  />} pass={"https://github.com/bogdandraghia"} />
-            </div>
-
+          </div>
+          <div className={style.iconComponent}>
             <SvgIconHelper name="email" icon={<Twitter  />} pass={"https://github.com/bogdandraghia"} />
-            </div>
+          </div>
 
-          </div>
-          <div className={style.formBox}>
-            <form className={style.form}>
-              <div className={style.formWrap}>
-                <label htmlFor="formname">
-                  Name
-                </label>
-                <input
-                  type="text"
-                  id="formname"
-                  required
-                />
-              </div>
-              <div className={style.formWrap}>
-                <label htmlFor="formname">
-                  Email
-                </label>
-                <input
-                  type="text"
-                  id="formname"
-                  required
-                />
-              </div>
-              <div className={style.formWrap}>
-                <label htmlFor="formname">
-                  Message
-                </label>
-                <textarea
-                  type="textarea"
-                  id="formname"
-                  required
-                />
-              </div>
-              <button className={style.buttonForm}>
-                Lorem ipsum
-              </button>
-            </form>
-          </div>
         </div>
+        <div className={style.formBox}>
+          <form className={style.form}>
+            <div className={style.formWrap}>
+              <label htmlFor="formname">
+                Name
+              </label>
+              <input
+                type="text"
+                id="formname"
+                required
+              />
+            </div>
+            <div className={style.formWrap}>
+              <label htmlFor="formname">
+                Email
+              </label>
+              <input
+                type="text"
+                id="formname"
+                required
+              />
+            </div>
+            <div className={style.formWrap}>
+              <label htmlFor="formname">
+                Message
+              </label>
+              <textarea
+                type="textarea"
+                id="formname"
+                required
+              />
+            </div>
+            <button className={style.buttonForm}>
+              Lorem ipsum
+            </button>
+          </form>
+        </div>
+      </div>
+
       </div>
     </div>
 

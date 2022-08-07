f73f51a4f53c75f9ca42eacb40f1608079a64e8c
bogdandraghia <bgd.draghia@gmail.com>
2022-08-07 22:05:23 +0000

    fix export static website,added next-export-optimize-images

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
-module.exports = {
+const withExportImages = require('next-export-optimize-images')
+
+module.exports = withExportImages({
   images: {
     domains: ["172.20.0.5","s3-provider-strapi-portofolio.s3.eu-west-3.amazonaws.com"],
   },
-}
+})

4bc32d2703815a0576d0a1eda38bdf4094bc042d
bogdandraghia <bgd.draghia@gmail.com>
2022-11-06 14:40:42 +0000

    modified design of blog items

--- pages/blog.js
+++ pages/blog.js
@@ -3,84 +3,44 @@ import Link from "next/link";
 import { motion } from "framer-motion";
 import axios from "axios";
 
-const BlogHighLight = ({ dataPost, highlight = true }) => {
-  console.log(dataPost);
+const BlogItem = (props) => {
   return (
-    <Link href={"/blog/" + dataPost.attributes.slug} passHref>
-      <a
-        className={style.lastPostSection}
-        style={!highlight ? { boxShadow: "none" } : {}}
-      >
-        <div className={style.blogItemLastImage}>
-          <img
-            src={dataPost.attributes.thumbnailUrl}
-            alt="mockphoto"
-            width="400px"
-            height="300px"
-          />
-        </div>
-        <div className={style.blogItemLastText}>
-          <h2>{dataPost.attributes.title}</h2>
-          {dataPost.attributes.content.length > 20 ? (
+    <Link href={"/blog/" + props.attributes.slug} passHref>
+      <div className={style.blogItem}>
+        <div className={style.blogItemContent}>
+          <div className={style.blogItemImage}>
+            <img
+              src={props.attributes.thumbnailUrl}
+              width="400px"
+              height="300px"
+              alt="imageblog"
+            />
+          </div>
+          <h3>{props.attributes.title}</h3>
+          {props.attributes.content.length > 100 ? (
             <div>
-              {dataPost.attributes.content.slice(
+              {props.attributes.content.slice(
                 0,
-                dataPost.attributes.content.slice(0, 120).lastIndexOf(" ")
+                props.attributes.content.slice(0, 130).lastIndexOf(" ")
               ) + "..."}
             </div>
           ) : (
-            <div>{dataPost.attributes.content}</div>
+            <div>{props.attributes.content}</div>
           )}
-          <button className={style.blogbutton}>Read more</button>
         </div>
-      </a>
+      </div>
     </Link>
   );
 };
 
-const BlogItem = ({ dataPost }) => {
-  return (
-    <Link href={"/blog/" + dataPost.attributes.slug} passHref>
-      <motion.a whileHover={{ scale: 1.02 }} className={style.blogItem}>
-        <div className={style.blogItemImage}>
-          <img
-            src={dataPost.attributes.thumbnailUrl}
-            width="400px"
-            height="300px"
-            alt="profile"
-          />
-        </div>
-        <div className={style.blogItemText}>
-          <h1>{dataPost.attributes.title}</h1>
-          {dataPost.attributes.content.length > 100 ? (
-            <div>
-              {dataPost.attributes.content.slice(
-                0,
-                dataPost.attributes.content.slice(0, 130).lastIndexOf(" ")
-              ) + "..."}
-            </div>
-          ) : (
-            <div>{dataPost.attributes.content}</div>
-          )}
-        </div>
-      </motion.a>
-    </Link>
-  );
-};
-
-const BlogContainer = ({ posts, backendUrl }) => {
+const BlogContainer = ({ posts }) => {
   return (
     <div className={style.wrapBlog}>
       <div className={style.centerSection}>
         <div className={style.subContentContainer}>
           <div className={style.postSection}>
             {posts.slice(1).map((post, index) => (
-              <BlogHighLight
-                dataPost={post}
-                backendUrl={backendUrl}
-                highlight={false}
-                key={index}
-              />
+              <BlogItem {...post} key={index} />
             ))}
           </div>
         </div>
@@ -92,13 +52,11 @@ const BlogContainer = ({ posts, backendUrl }) => {
 export default BlogContainer;
 
 export const getStaticProps = async () => {
-  const backendUrl = process.env.BACKEND_URL;
   const res = await axios.get(process.env.BACKEND_URL + "/api/articles");
   console.log(res.data.data);
   const posts = res.data.data;
   return {
     props: {
-      backendUrl,
       posts,
     },
   };
